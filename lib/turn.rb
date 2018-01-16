97a9e6d4c14c22654658e5349ef61fab026cdac7
Stephen Thurman <thurman024@gmail.com>
2018-01-16 17:36:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,9 +30,10 @@ def position_taken?(board, index)
   end
 end
 
-def turn(board, index)
+def turn(board)
   puts "Please enter 1-9"
   input = gets.strip
+  index = input_to_index(input)
   if valid_move?(board, index)
     #update board with new move
   else
